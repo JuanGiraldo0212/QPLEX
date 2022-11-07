644b42d6439e902b19605d2abd97f4932d66c34f
Jose Ossorio <joseossorio@hotmail.com>
2022-11-07 14:58:35 -0800

    add code to create dictionary with model info. Missing variable types

--- qplex/library/qmodel.py
+++ qplex/library/qmodel.py
@@ -9,8 +9,8 @@ class QModel(Model):
     def solve(self, solver: str = 'classical'):
         # Use this information to create a dictionary with all the information about the model, then pass it to the
         # solver request. Must include, name, variables (with types), constraints and objective function.
-        for v in self.iter_variables():
-            print(f"{v}")
+        # for v in self.iter_variables():
+        #     print(f"{v}")
         # for c in self.iter_constraints():
         #     print(c)
         # print(self.get_objective_expr())
@@ -21,9 +21,15 @@ class QModel(Model):
             Model.solve(self)
         elif solver == 'quantum':
             model = {
-                'name': 'peti',
-
+                'name': self.name,
+                'variables': list(map(lambda var: {
+                    'name': var.name,
+                    'type': var.type if hasattr(var, 'type') else '',
+                }, list(self.iter_variables()))),
+                'constraints': list(map(lambda constraint: constraint.to_string(), list(self.iter_constraints()))),
+                'objective_func': self.get_objective_expr().to_string(),
+                'sense': self.objective_sense.name,
             }
-            print(solver_request())
+            print(solver_request(model=model))
         else:
             raise ValueError("Invalid value for argument 'hardware'")
