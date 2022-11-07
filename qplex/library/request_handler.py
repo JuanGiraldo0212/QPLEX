974158e94a72d707ffa22464fbd053e5902915ce
Jose Ossorio <joseossorio@hotmail.com>
2022-11-07 14:59:12 -0800

    remove unncecessary toJSON function

--- qplex/library/request_handler.py
+++ qplex/library/request_handler.py
@@ -5,11 +5,6 @@ BACKEND_BASE_ULR = "https://3eff41c5-b8bf-4056-94f9-e2712b151c0a.mock.pstmn.io/"
 SOLVE_URI = BACKEND_BASE_ULR + "solve"
 
 
-def toJSON(el):
-    return json.dumps(el, default=lambda o: o.__dict__,
-                      sort_keys=True, indent=4)
-
-
 def solver_request(model):
     request_body = model
     print(request_body)
