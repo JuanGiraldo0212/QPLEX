45f560147b4c9f5728e469a02f6d52cda0fc64af
Jose Ossorio <joseossorio@hotmail.com>
2022-11-07 18:00:52 -0800

    corrections

--- qplex/library/request_handler.py
+++ qplex/library/request_handler.py
@@ -1,12 +1,9 @@
 import requests
-import json
 
 BACKEND_BASE_ULR = "https://3eff41c5-b8bf-4056-94f9-e2712b151c0a.mock.pstmn.io/"
 SOLVE_URI = BACKEND_BASE_ULR + "solve"
 
 
 def solver_request(model):
-    request_body = model
-    print(request_body)
-    request = requests.post(SOLVE_URI, json=request_body)
+    request = requests.post(SOLVE_URI, json=model)
     return request.json() if request.status_code == requests.codes.ok else request.status_code
