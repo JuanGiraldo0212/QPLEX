f76e489a7c11563ca6f92a4bfe5cb2f085db4d89
Jose Manuel Ossorio <37313690+jmossorio99@users.noreply.github.com>
2022-11-07 18:04:10 -0800

    Override solve (#1)
    * if check for 'solver' value
    
    * add code to create dictionary with model info. Missing variable types
    
    * remove unncecessary toJSON function
    
    * add variable types
    
    * corrections
    
    Co-authored-by: Jose Ossorio <joseossorio@hotmail.com>

--- qplex/library/qmodel.py
+++ qplex/library/qmodel.py
@@ -5,19 +5,23 @@ from .request_handler import solver_request
 class QModel(Model):
     def __init__(self, name):
         super(QModel, self).__init__(name)
-        
-    def solve(self):
-        # Use this information to create a dictionary with all the information about the model, then pass it to the
-        # solver request. Must include, name, variables (with types), constraints and objective function.
-        # for v in self.iter_variables():
-        #     print(f"{v}")
-        # for c in self.iter_constraints():
-        #     print(c)
-        # print(self.get_objective_expr())
-        # print(self.objective_sense.name)
 
-        print(solver_request())
+    def build_model_dict(self):
+        return {
+            'name': self.name,
+            'variables': list(map(lambda var: {
+                'name': var.name,
+                'type': var.vartype.cplex_typecode,
+            }, list(self.iter_variables()))),
+            'constraints': list(map(lambda constraint: constraint.to_string(), list(self.iter_constraints()))),
+            'objective_func': self.get_objective_expr().to_string(),
+            'sense': self.objective_sense.name,
+        }
 
-        Model.solve(self)
-
-
+    def solve(self, solver: str = 'classical'):
+        if solver == 'classical':
+            Model.solve(self)
+        elif solver == 'quantum':
+            print(solver_request(model=self.build_model_dict()))
+        else:
+            raise ValueError("Invalid value for argument 'hardware'")
--- qplex/library/request_handler.py
+++ qplex/library/request_handler.py
@@ -4,7 +4,6 @@ BACKEND_BASE_ULR = "https://3eff41c5-b8bf-4056-94f9-e2712b151c0a.mock.pstmn.io/"
 SOLVE_URI = BACKEND_BASE_ULR + "solve"
 
 
-def solver_request():
-    request_body = {'test': 123456}  # Have to create an argument that has all the information about the model
-    request = requests.post(SOLVE_URI, json=request_body)
+def solver_request(model):
+    request = requests.post(SOLVE_URI, json=model)
     return request.json() if request.status_code == requests.codes.ok else request.status_code
