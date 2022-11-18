bec301509ae0a933e4551a62e538015908ca1558
Jose Manuel Ossorio <37313690+jmossorio99@users.noreply.github.com>
2022-11-17 18:48:04 -0800

    add parsing of backend response, asJob and backend parameters (#2)
    Co-authored-by: Jose Ossorio <joseossorio@hotmail.com>

--- qplex/library/request_handler.py
+++ qplex/library/request_handler.py
@@ -4,6 +4,7 @@ BACKEND_BASE_ULR = "https://3eff41c5-b8bf-4056-94f9-e2712b151c0a.mock.pstmn.io/"
 SOLVE_URI = BACKEND_BASE_ULR + "solve"
 
 
-def solver_request(model):
-    request = requests.post(SOLVE_URI, json=model)
+def solver_request(model, as_job, backend):
+    params = {'asJob': as_job}
+    request = requests.post(SOLVE_URI + (f'/{backend}' if backend else ''), params=params, json=model)
     return request.json() if request.status_code == requests.codes.ok else request.status_code
