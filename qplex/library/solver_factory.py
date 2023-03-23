e78fef628f2d9bd3a7dfa0152a8562f2389e9d26
José Ossorio <37313690+jmossorio99@users.noreply.github.com>
2023-03-23 11:27:46 -0700

    fix d-wave typo

--- qplex/library/solver_factory.py
+++ qplex/library/solver_factory.py
@@ -3,7 +3,7 @@ from qplex.library.gate_based_solver import GateBasedSolver
 
 class SolverFactory:
     def get_solver(self, backend):
-        if backend == 'dwave':
+        if backend == 'd-wave':
             return DWaveSolver()
         if backend == 'ibm':
             return GateBasedSolver()
