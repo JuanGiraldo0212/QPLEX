5ce3a32783da8461a7af210788743aae0eb1611e
jmossorio99 <joseossorio99@hotmail.com>
2023-05-13 18:02:25 -0700

    10-feature-refactor-code-to-match-the-correct-language

--- qplex/helpers/factories/solver_factory.py
+++ qplex/helpers/factories/solver_factory.py
@@ -3,22 +3,22 @@ from qplex.solvers import DWaveSolver, GateBasedSolver
 
 class SolverFactory:
     @staticmethod
-    def get_solver(backend: str, quantum_api_tokens: dict):
-        if backend is None:
+    def get_solver(provider: str, quantum_api_tokens: dict):
+        if provider is None:
             if quantum_api_tokens.get("dwave_token"):
                 return DWaveSolver()
             if quantum_api_tokens.get("ibm_token"):
                 return GateBasedSolver()
             raise RuntimeError("Missing credentials for quantum provider")
-        if backend == 'd-wave':
+        if provider == 'd-wave':
             if quantum_api_tokens.get("dwave_token") is None:
                 raise RuntimeError("Missing credentials for the D-Wave provider")
             return DWaveSolver()
-        if backend == 'ibm':
+        if provider == 'ibm':
             if quantum_api_tokens.get("ibm_token") is None:
                 raise RuntimeError("Missing credentials for the IBM provider")
             return GateBasedSolver()
-        raise ValueError(backend)
+        raise ValueError(provider)
 
 
 solver_factory = SolverFactory()
