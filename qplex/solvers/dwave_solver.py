3ead9ea3540f6bad7414277f7e1548c5649c007e
José Ossorio <37313690+joseosso@users.noreply.github.com>
2023-10-07 13:02:56 -0700

    Add logic to pick the right d-wave hybrid solver (#24)

--- qplex/solvers/dwave_solver.py
+++ qplex/solvers/dwave_solver.py
@@ -1,22 +1,27 @@
 from typing import Dict, Any
 
-from docplex.mp.linear import LinearExpr
-
 from qplex.model.constants import VAR_TYPE
-from dwave.system import LeapHybridCQMSampler
-from dimod import ConstrainedQuadraticModel, QuadraticModel
+from dwave.system import LeapHybridCQMSampler, LeapHybridBQMSampler, LeapHybridDQMSampler
+from dimod import ConstrainedQuadraticModel, QuadraticModel, DiscreteQuadraticModel, BinaryQuadraticModel
 from qplex.solvers.base_solver import Solver
 
 
 class DWaveSolver(Solver):
 
     def solve(self, model) -> dict:
-        token = model.quantum_api_tokens.get("dwave_token")
-        sampler = LeapHybridCQMSampler(token=token)
-        cqm = self.parse_input(model)
-        sampleset = sampler.sample_cqm(cqm, label=model.name)
-        feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)
-        best = feasible_sampleset.first
+        token = model.quantum_api_tokens.get("d-wave_token")
+        parsed_model, model_type = self.parse_input(model)
+        if model_type == VAR_TYPE['C']:
+            sampler = LeapHybridCQMSampler(token=token)
+            sampleset = sampler.sample_cqm(parsed_model, label=model.name).filter(lambda row: row.is_feasible)
+            print(sampleset)
+        elif model_type == VAR_TYPE['I']:
+            sampler = LeapHybridDQMSampler(token=token)
+            sampleset = sampler.sample_dqm(parsed_model, label=model.name)
+        else:
+            sampler = LeapHybridBQMSampler(token=token)
+            sampleset = sampler.sample(parsed_model, label=model.name)
+        best = sampleset.first
         response = self.parse_response(best)
         return response
 
@@ -28,58 +33,63 @@ class DWaveSolver(Solver):
 
         return result
 
-    def parse_input(self, model) -> ConstrainedQuadraticModel:
+    def parse_input(self, model) -> any:
 
-        cqm = ConstrainedQuadraticModel()
-        obj = QuadraticModel()
-
-        for var in model.iter_variables():
-            obj.add_variable(VAR_TYPE[var.vartype.cplex_typecode], var.name, lower_bound=var.lb,
-                             upper_bound=var.ub)
-
-        if type(model.get_objective_expr()) is LinearExpr:
-            terms = model.get_objective_expr().iter_terms()
-            for t in terms:
-                value = t[1] if model.objective_sense.name == "Minimize" else t[1] * -1
-                obj.set_linear(t[0].name, value)
+        if len(list(model.iter_constraints())) > 0:
+            model_type = VAR_TYPE['C']
+            obj = self.parse_objective(model, QuadraticModel())
+            parsed_model = ConstrainedQuadraticModel()
+            parsed_model.set_objective(obj)
+            for constraint in model.iter_constraints():
+                const_qm = self.parse_constraint(constraint)
+                sense = constraint.sense.operator_symbol
+                rhs = constraint.right_expr.constant
+                parsed_model.add_constraint(const_qm, sense=sense, rhs=rhs, label=constraint.lpt_name)
         else:
-            linear_terms = model.get_objective_expr().iter_terms()
-            quadratic_terms = list(model.get_objective_expr().iter_quad_triplets())
-            if len(linear_terms) > 0:
-                for t in linear_terms:
-                    value = t[1] if model.objective_sense.name == "Minimize" else t[1] * -1
-                    obj.set_linear(t[0].name, value)
-            if len(quadratic_terms) > 0:
-                for t in quadratic_terms:
-                    value = t[2] if model.objective_sense.name == "Minimize" else t[2] * -1
-                    obj.set_quadratic(t[0].name, t[1].name, value)
+            model_type = VAR_TYPE['B']
+            if any(VAR_TYPE[var.vartype.cplex_typecode] == VAR_TYPE['I'] for var in model.iter_variables()):
+                model_type = VAR_TYPE['I']
+            parsed_model = DiscreteQuadraticModel() if model_type == VAR_TYPE['I'] else BinaryQuadraticModel(
+                vartype='BINARY')
+            parsed_model = self.parse_objective(model, parsed_model)
 
-        cqm.set_objective(obj)
+        return parsed_model, model_type
 
-        for const in list(map(lambda constraint: constraint, list(model.iter_constraints()))):
-            const_qm = QuadraticModel()
+    def parse_objective(self, model, parsed_model):
+        if type(parsed_model) is BinaryQuadraticModel:
             for var in model.iter_variables():
-                const_qm.add_variable(VAR_TYPE[var.vartype.cplex_typecode], var.name, lower_bound=var.lb,
-                                      upper_bound=var.ub)
-            expr = const.left_expr
-            value = const.right_expr.constant
-            sen = const.sense.operator_symbol
-            if type(expr) is LinearExpr:
-                terms = expr.iter_terms()
-                for t in terms:
-                    const_qm.set_linear(t[0].name, t[1])
-            else:
-                linear_terms = expr.iter_terms()
-                quadratic_terms = list(expr.iter_quad_triplets())
-                if len(linear_terms) > 0:
-                    for t in linear_terms:
-                        const_qm.set_linear(t[0].name, t[1])
-                if len(quadratic_terms) > 0:
-                    for t in quadratic_terms:
-                        const_qm.set_quadratic(t[0].name, t[1].name, t[2])
-            cqm.add_constraint(const_qm, sense=sen, rhs=value, label=const.lpt_name)
+                parsed_model.add_variable(var.name)
+        else:
+            for var in model.iter_variables():
+                parsed_model.add_variable(VAR_TYPE[var.vartype.cplex_typecode], var.name, lower_bound=var.lb,
+                                          upper_bound=var.ub)
 
-        return cqm
+        linear_terms = model.get_objective_expr().iter_terms()
+        sense_multiplier = 1 if model.objective_sense.name == "Minimize" else -1
+
+        for term in linear_terms:
+            parsed_model.set_linear(term[0].name, term[1] * sense_multiplier)
+
+        for term in model.get_objective_expr().iter_quad_triplets():
+            parsed_model.set_quadratic(term[0].name, term[1].name, term[2] * sense_multiplier)
+
+        return parsed_model
+
+    def parse_constraint(self, constraint):
+        const_qm = QuadraticModel()
+        for var in constraint.iter_variables():
+            const_qm.add_variable(VAR_TYPE[var.vartype.cplex_typecode], var.name, lower_bound=var.lb,
+                                  upper_bound=var.ub)
+
+        expr = constraint.left_expr
+
+        for term in expr.iter_terms():
+            const_qm.set_linear(term[0].name, term[1])
+
+        for term in expr.iter_quad_triplets():
+            const_qm.set_quadratic(term[0].name, term[1].name, term[2])
+
+        return const_qm
 
     def select_backend(self, model) -> str:
         pass
