468048558ee1cc87b1504ee33e4bbe8b45406f4c
Juan Giraldo <juan.giraldo@multiversecomputing.com>
2023-10-15 12:04:31 -0700

    Added support for simulators and quantum backends

--- qplex/commons/solver_factory.py
+++ qplex/commons/solver_factory.py
@@ -6,20 +6,22 @@ from qplex.solvers.braket_solver import BraketSolver
 class SolverFactory:
     @staticmethod
     def get_solver(provider: str, quantum_api_tokens: dict, shots: int, backend: str):
+        dwave_token = quantum_api_tokens.get("dwave_token")
+        ibmq_token = quantum_api_tokens.get("ibmq_token")
         if provider is None:
-            if quantum_api_tokens.get("dwave_token"):
+            if dwave_token:
                 return DWaveSolver()
-            if quantum_api_tokens.get("ibm_token"):
-                return IBMQSolver(shots=shots, backend=backend)
+            if ibmq_token:
+                return IBMQSolver(token=ibmq_token, shots=shots, backend=backend)
             raise RuntimeError("Missing credentials for quantum provider")
         if provider == 'd-wave':
-            if quantum_api_tokens.get("dwave_token") is None:
+            if dwave_token is None:
                 raise RuntimeError("Missing credentials for the D-Wave provider")
             return DWaveSolver()
         if provider == 'ibmq':
-            if quantum_api_tokens.get("ibmq_token") is None:
+            if ibmq_token is None:
                 raise RuntimeError("Missing credentials for the IBM provider")
-            return IBMQSolver(shots=shots, backend=backend)
+            return IBMQSolver(token=ibmq_token, shots=shots, backend=backend)
         if provider == "braket":
             return BraketSolver(shots=shots, backend=backend)
         raise ValueError(provider)
