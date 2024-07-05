39a11ac34f2330b10c5feee6d09da6b84e1f71b6
joseosso <joseossorio99@hotmail.com>
2024-07-05 16:48:20 -0700

    remove problem specific models

--- examples/knapsack.py
+++ examples/knapsack.py
@@ -1,5 +1,18 @@
-from qplex import KnapsackModel
+from typing import List
+from qplex import QModel
 from qplex.model import Options
+
+
+def model_knapsack_problem(values: List, weights: List, const: int) -> QModel:
+    n_items = len(values)
+    knapsack_model = QModel('knapsack')
+    x = knapsack_model.binary_var_list(n_items, name="x")
+    knapsack_model.add_constraint(
+        sum(weights[i] * x[i] for i in range(n_items)) <= const)
+    obj_fn = sum(values[i] * x[i] for i in range(n_items))
+    knapsack_model.set_objective('max', obj_fn)
+
+    return knapsack_model
 
 
 def main():
@@ -7,7 +20,7 @@ def main():
     weights = [4, 2, 5, 4, 5, 1, 3, 5]
     const = 15
 
-    knapsack_model = KnapsackModel('knapsack', values, weights, const)
+    knapsack_model = model_knapsack_problem(values, weights, const)
 
     execution_params = {
         "provider": "ibmq",
--- qplex/model/__init__.py
+++ qplex/model/__init__.py
@@ -3,6 +3,4 @@ This module provides the qmodel for the definition of an optimization problem mo
 """
 
 from qplex.model.qmodel import QModel
-from qplex.model.knapsack_model import KnapsackModel
-from qplex.model.max_cut_model import MaxCutModel
 from qplex.model.options import Options
